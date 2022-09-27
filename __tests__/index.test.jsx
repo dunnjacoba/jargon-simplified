41d2db008b99ee06c7680b8f0259d4f32f895d53
Jacob Dunn <101910318+dunnjacoba@users.noreply.github.com>
2022-09-27 11:35:44 -0500

    Dunnjacoba changes (#27)
    * [Docs] updates README
    
    * [Fix] Adds status
    
    * [Update] adds Sqlite
    
    * [Update] Creates put for db
    
    * [Update] testing

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,20 +1,14 @@
-import { cleanup, render, screen } from "@testing-library/react";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
 import HelloWorld from "../pages/helloworld";
 
 afterAll(cleanup);
 
-describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />);
-
-    const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
-    });
-
-    expect(heading).toBeInTheDocument();
-  });
+test("renders a heading", () => {
+  render(<Home />);
+  const heading = screen.findByTestId("paragraph-one");
+  expect(heading);
 });
 
 test("is on HelloWorld", async () => {
@@ -27,4 +21,10 @@ test("is on HelloWorld", async () => {
   render(<HelloWorld />);
   const button = await screen.findAllByRole("button");
   expect(button).toHaveLength(2);
+});
+
+test("clicked Click Me", () => {
+  render(<HelloWorld />);
+  const clickME = screen.getByText("Click Here");
+  fireEvent.click(clickME);
 });
