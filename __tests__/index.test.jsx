7fbe538b0dfc1fbb35b7c518e1fcf362e79519b2
Jacob Dunn <101910318+dunnjacoba@users.noreply.github.com>
2022-10-05 11:16:52 -0500

    Dunnjacoba changes (#43)
    * [Docs] updates README
    
    * [Fix] Adds status
    
    * [Update] adds Sqlite
    
    * [Update] Creates put for db
    
    * [Update] testing
    
    * update: adds axios library
    
    * merge remote main
    
    * fix: axios utilization
    
    * feature: adds component JargonCard
    
    * feature: adds github actions
    
    * fix: changed naming convention
    
    * fix: corrects typo in yml
    
    * fix: changes naming convention in yml
    
    * fix: adds tests to github actions
    
    * fix: node version in yml
    
    * update: Actions
    
    * ci: Updates yml
    
    * ci: Updates yml
    
    * update: removes custom404
    
    * ci: Update env
    
    * ci: updated
    
    * docs: updates README
    
    * docs: updates README
    
    * feature: Adds searchbar
    
    * test: Simplifies testing for CI/CD setup

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,30 +1,11 @@
-import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
-import HelloWorld from "../pages/helloworld";
 
 afterAll(cleanup);
 
-test("renders a heading", () => {
+test("renders a button", () => {
   render(<Home />);
-  const heading = screen.findByTestId("paragraph-one");
-  expect(heading);
-});
-
-test("is on HelloWorld", async () => {
-  render(<HelloWorld />);
-  const button = await screen.findAllByRole("button");
-  expect(button[0]).toHaveProperty("type", "button");
-});
-
-test("is on HelloWorld", async () => {
-  render(<HelloWorld />);
-  const button = await screen.findAllByRole("button");
-  expect(button).toHaveLength(2);
-});
-
-test("clicked Click Me", () => {
-  render(<HelloWorld />);
-  const clickME = screen.getByText("Click Here");
-  fireEvent.click(clickME);
+  const found = screen.findByRole("button");
+  expect(found);
 });
